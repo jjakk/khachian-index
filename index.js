027b5cb2bda38464b8756acf82c5d6b80fa430b0
John Khachian <jaksawesome10@gmail.com>
2022-08-07 21:15:14 -0400

    home page

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const express = require("express");
 const Alpaca = require("@alpacahq/alpaca-trade-api");
 const dotenv = require("dotenv");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Setup
 dotenv.config();
+app.set('view engine', 'pug')
+app.use(express.static('public'))
 
 const alpaca = new Alpaca({
     keyId: process.env.ALPACA_KEY,
@@ -8,32 +15,30 @@ const alpaca = new Alpaca({
     paper: true
 });
 
-(async function(){
-    try{
-        const stocksToBy = [
-            "AAPL",
-            "TSLA",
-            "AMZN",
-            "GOOGL",
-            "FB",
-            "NFLX"
-        ];
-    
-        const account = await alpaca.getAccount();
-        //console.log(`Buying power: ${account.buying_power}`);
-    
-        /*const order = await alpaca.createOrder({
-            symbol: stocksToBy[0],
-            notion: 0.1,
-            side: "buy",
-            type: "market",
-            time_in_force: "day"
-        });
-    
-        response.send(order);*/
-        console.log(account);
-    }
-    catch(error){
-        console.log(error.message);
-    }
-})();
+app.get("/", async (req, res) => {
+    const account = await alpaca.getAccount();
+    res.render('index', {
+        account: {
+            ...account,
+        }
+    });
+});
+
+app.put("/update", async (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+/*
+// Order code
+const order = await alpaca.createOrder({
+    symbol: "MSFT",
+    notional: 1,
+    side: "buy",
+    type: "market",
+    time_in_force: "day"
+});
+*/
