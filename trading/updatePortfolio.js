82912dfbeab3796e303ae22c74c40f6a24ea7b8b
John Khachian <jaksawesome10@gmail.com>
2022-08-20 11:06:51 -0400

    bug fix

--- trading/updatePortfolio.js
+++ trading/updatePortfolio.js
@@ -12,7 +12,6 @@ const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
 (async function(){
     try{
-        const account = await alpaca.getAccount();
         let allSymbols = await csv().fromFile("./indexAlgorithm/allTickers.csv");
         allSymbols = allSymbols
             .sort((a,b) => parseInt(b["Market Cap"] || 0) - parseInt(a["Market Cap"] || 0))
@@ -20,27 +19,22 @@ const sleep = time => new Promise(resolve => setTimeout(resolve, time));
             .slice(0,100);
         let scores = await getScores(allSymbols);
         await alpaca.closeAllPositions();
+        const account = await alpaca.getAccount();
         const cash = account.cash;
         const scoreSum = scores.reduce((t,b)=>t+parseFloat(b.score || 0),0);
         for(const score of scores){
-            console.log(score.symbol, score.score);
             const portfolioDiversity = (score.score||0)/scoreSum;
-            console.log({
-                symbol: score.symbol,
-                portfolioDiversity: portfolioDiversity,
-                cash: cash,
-                side: "buy",
-                type: "market",
-                time_in_force: "day"
-            });
-            await alpaca.createOrder({
-                symbol: score.symbol,
-                notional: portfolioDiversity*cash,
-                side: "buy",
-                type: "market",
-                time_in_force: "day"
-            });
-            console.log(score.symbol, portfolioDiversity*cash);
+            const purchaseCost = portfolioDiversity*cash;
+            if(purchaseCost > 0){
+                await alpaca.createOrder({
+                    symbol: score.symbol,
+                    notional: purchaseCost,
+                    side: "buy",
+                    type: "market",
+                    time_in_force: "day"
+                });
+            }
+            console.log(score.symbol, purchaseCost);
         }
         console.log("Portfolio updated");
     }
